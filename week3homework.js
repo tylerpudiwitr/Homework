8e86ca77d589485dc49bac39c0bf622371034b86
tylerpudiwitr <tyler.pudiwitr@outlook.com>
2022-09-06 15:34:40 -0500

    week3hw

---
+++ week3homework.js
@@ -0,0 +1,40 @@
+// Array of toppings
+const pizzaToppings = ["pepperoni", "jalapenos", "bacon", "pineapple"];
+
+// Customer greeting function
+funciton greetCustomer() {
+  console.log("Welcome to Tyler' Pizza, we now have toppings such as " + pizzaToppings + "!");
+  for (let toppings of pizzaToppings) {
+    console.log(pizzaToppings);
+  }
+  return pizzaToppings;
+}
+greetCustomer();
+
+// getPizzaOrder Function
+let pizzaObject = {
+  size: "large",
+  crust: "thin",
+  toppings: pizzaToppings,
+};
+return pizzaObject;
+
+function getPizzaOrder() {
+  for (let toppings of pizzaToppings) {
+    console.log(`One ${pizzaObject.size} ${pizzaObject.crust} pizza with ${pizzaObject.toppings} coming up!`);
+  };
+return pizzaOrder = {size, crust, ...toppings};
+
+// preparePizza function
+function preparePizza();
+  console.log(`One ${large} ${crust} pizza with ${toppings} is now in the oven!`);
+  return preparePizza;
+}
+
+// servePizza function
+function servePizza() {
+  for (let toppings of pizzaToppings) {
+    console.log("Order up! Here's your ${pizzaObject.size} ${pizzaObject.crust} pizza with ${pizzaObject.toppings}, enjoy!");
+  }
+return servePizza;
+
