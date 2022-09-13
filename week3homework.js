2c4b9dc4a4d4e2ccedf79d54c8061f4669bbf14f
tylerpudiwitr <tyler.pudiwitr@outlook.com>
2022-09-13 18:20:31 -0500

    week3hwRevised

--- week3homework.js
+++ week3homework.js
@@ -2,37 +2,34 @@
 const pizzaToppings = ["pepperoni", "jalapenos", "bacon", "pineapple"];
 
 // Customer greeting function
-funciton greetCustomer() {
-  console.log("Welcome to Tyler' Pizza, we now have toppings such as " + pizzaToppings + "!");
-  for (let toppings of pizzaToppings) {
-    console.log(pizzaToppings);
+function greetCustomer() {
+  console.log(`Welcome to Tyler's Pizza, we now have toppings such as; `);
+  for (let topping of toppings) {
+    greeting += `${topping}`;
   }
-  return pizzaToppings;
+  console.log(greeting);
 }
 greetCustomer();
 
 // getPizzaOrder Function
-let pizzaObject = {
-  size: "large",
-  crust: "thin",
-  toppings: pizzaToppings,
-};
-return pizzaObject;
-
-function getPizzaOrder() {
+function getPizzaOrder(size, crust, ...toppings) {
   for (let toppings of pizzaToppings) {
     console.log(`One ${pizzaObject.size} ${pizzaObject.crust} pizza with ${pizzaObject.toppings} coming up!`);
   };
-return pizzaOrder = {size, crust, ...toppings};
+return pizzaOrder = {
+  size:orderSize,
+  crust:orderCrust,
+  toppings: orderToppings,
+};
 
 // preparePizza function
-function preparePizza();
+function preparePizza([orderSize, orderCrust, orderToppings]);
   console.log(`One ${large} ${crust} pizza with ${toppings} is now in the oven!`);
   return preparePizza;
 }
 
 // servePizza function
-function servePizza() {
+function servePizza(pizza) {
   for (let toppings of pizzaToppings) {
     console.log("Order up! Here's your ${pizzaObject.size} ${pizzaObject.crust} pizza with ${pizzaObject.toppings}, enjoy!");
   }
